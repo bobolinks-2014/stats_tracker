7f5f83c38c78ec633782adae0e910084508904be
mag42c <michael.ginalick@gmail.com>
2014-09-05 18:40:03 -0500

    create_games_done

--- db/migrate/20140904135337_create_games.rb
+++ db/migrate/20140904135337_create_games.rb
@@ -2,7 +2,8 @@ class CreateGames < ActiveRecord::Migration
   def change
     create_table :games do |t|
       t.datetime :date
-      t.string :location, :default => "home", :opponent
+      t.string :location, :default => "home"
+      t.string :opponent
       t.boolean :win, :default => nil
       t.integer :team_score, :default => nil
       t.integer :opponent_score, :default => nil
