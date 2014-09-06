b89fb25b57a588ce8d354919369512103c3cd6b6
David Nanry <dnanry@gmail.com>
2014-09-06 14:33:51 -0500

    Dashboard: multiple rows can open; refactored DCtrl functions; added comments

--- app/assets/javascripts/angular/controllers/DashboardCtrl.js
+++ app/assets/javascripts/angular/controllers/DashboardCtrl.js
@@ -2,151 +2,145 @@
 
 // ]
 
-	app.controller('DashboardCtrl', ['$http',function($http){
-	// function DashboardCtrl($scope){
-		this.row = 0;
-		this.team_id = null;
-		this.season_id = null;
+app.controller('DashboardCtrl', ['$http',function($http){
+//  === SET ZE VARS ===
+// 
+	// Set Teams row to auto-display
+	this.showRow = {'teams': true, 'seasons': false, 'games': false};
 
-		this.selectRow = function(setRow){
-			this.row = setRow;
-		};
+	// Set these to view specific team's season or specific season's games
+	this.team_id = null;
+	this.season_id = null;
 
-		this.isSet = function(rowName){
-			return this.row === rowName;
-		};
-
-		this.showDerivative = function(idObj,setRow){
-			this.selectRow(setRow);
-			this.team_id = idObj.team;
-			this.season_id = idObj.season;
-		}
-		var that = this;
-		this.teams = [];
-		this.seasons = [];
-		// this.games = [];
-
-		// this.teams = data_teams;
-		// this.seasons = data_seasons;
-		this.games = data_games;
+	// User's data to display
+	this.teams = [];
+	this.seasons = [];
+	this.games = [];
 
 
-		//getting all the teams
-		$http({
+//  === HAVE ZE FUNCTIONS ===
+// 
+	// Oscillate between true / false
+	this.selectRow = function(rowName){
+		// debugger;
+		this.showRow[rowName] ? this.showRow[rowName] = false : this.showRow[rowName] = true;  
+	};
+
+	this.isSet = function(rowName){
+		return this.showRow[rowName];
+	};
+
+	this.showDerivative = function(idObj,rowName){
+		this.selectRow(rowName);
+		this.team_id = idObj.team || this.team_id;
+		this.season_id = idObj.season || this.season_id;
+	}
+	
+
+//  === GET ZE DATA ===
+// 
+	var that = this;
+
+	//getting all the teams
+	$http({
 			method: 'GET',
-			url: '/user/1/team/all.json'
-		}).
-		success(function(data, status, headers, config){
+			url: '/user/1/teams.json'
+		})
+		.success(function(data, status, headers, config){
 			that.teams = data;
-		}).
-		error(function(data, status, headers, config){
-			debugger;
+			// debugger;
 		})
+		.error(function(data, status, headers, config){
+			// debugger;
+	})	
 
-		//getting a season
-		$http({
+	//getting a season
+	$http({
 			method: 'GET',
-			url: '/season/team/1.json'
-		}).
-		success(function(data, status, headers, config){
+			url: '/user/1/team/1/seasons.json'
+		})	
+		.success(function(data, status, headers, config){
 			that.seasons = data;
-		}).
-		error(function(data, status, headers, config){
-			debugger;
+			// debugger;
+		})
+		.error(function(data, status, headers, config){
+			// debugger;
 		})
 
-		//getting a game
-		$http({
+	//getting a game
+	$http({
 			method: 'GET',
-			url: '/game/season/1.json'
-		}).
-		success(function(data, status, headers, config){
+			url: '/user/1/season/1/games.json'
+		})
+		.success(function(data, status, headers, config){
 			that.games = data;
-			debugger;
-		}).
-		error(function(data, status, headers, config){
-			debugger;
+			// debugger;
+		})
+		.error(function(data, status, headers, config){
+			// debugger;
 		})
 
+}]);
 
 
 
-
-
-
-
-	}]);
-
-
-
-	var data_seasons = [{
-		id: 3,
-		name: "2015",
-		team_id: 1
-	},
-	{
-		id: 2,
-		name: "2014",
-		team_id: 2
-	},
-	{
-		id: 1,
-		name: 2013,
-		team_id: 2
-	}]
-	var data_games = [{
-		date: "01/30",
-		location: "home",
-		opponent: "Pistons",
-		win: true,
-		team_score: 60,
-		opponent_score: 54,
-		season_id: 3
-	},
-	{
-		date: "01/28",
-		location: "home",
-		opponent: "Hawks",
-		win: false,
-		team_score: 40,
-		opponent_score: 54,
-		season_id: 3
-	},
-	{
-		date: "01/25",
-		location: "away",
-		opponent: "Knicks",
-		win: true,
-		team_score: 55,
-		opponent_score: 51,
-		season_id: 3
-	},
-	{
-		date: "01/20",
-		location: "home",
-		opponent: "Celtics",
-		win: true,
-		team_score: 50,
-		opponent_score: 43,
-		season_id: 3
-	},
-	{
-		date: "01/18",
-		location: "away",
-		opponent: "Hornets",
-		win: false,
-		team_score: 60,
-		opponent_score: 64,
-		season_id: 3
-	}]
-
-
-
-
-
-
-
-
-
-
-
+	// var data_seasons = [{
+	// 	id: 3,
+	// 	name: "2015",
+	// 	team_id: 1
+	// },
+	// {
+	// 	id: 2,
+	// 	name: "2014",
+	// 	team_id: 2
+	// },
+	// {
+	// 	id: 1,
+	// 	name: 2013,
+	// 	team_id: 2
+	// }]
+	// var data_games = [{
+	// 	date: "01/30",
+	// 	location: "home",
+	// 	opponent: "Pistons",
+	// 	win: true,
+	// 	team_score: 60,
+	// 	opponent_score: 54,
+	// 	season_id: 3
+	// },
+	// {
+	// 	date: "01/28",
+	// 	location: "home",
+	// 	opponent: "Hawks",
+	// 	win: false,
+	// 	team_score: 40,
+	// 	opponent_score: 54,
+	// 	season_id: 3
+	// },
+	// {
+	// 	date: "01/25",
+	// 	location: "away",
+	// 	opponent: "Knicks",
+	// 	win: true,
+	// 	team_score: 55,
+	// 	opponent_score: 51,
+	// 	season_id: 3
+	// },
+	// {
+	// 	date: "01/20",
+	// 	location: "home",
+	// 	opponent: "Celtics",
+	// 	win: true,
+	// 	team_score: 50,
+	// 	opponent_score: 43,
+	// 	season_id: 3
+	// },
+	// {
+	// 	date: "01/18",
+	// 	location: "away",
+	// 	opponent: "Hornets",
+	// 	win: false,
+	// 	team_score: 60,
+	// 	opponent_score: 64,
+	// 	season_id: 3
+	// }]
