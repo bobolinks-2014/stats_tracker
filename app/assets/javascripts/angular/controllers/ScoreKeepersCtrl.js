958595eb477ef004c573b22733f4fc9472ffb9af
Edgar Garza <egarza54@gmail.com>
2014-09-07 17:17:04 -0500

    score keeper js is updated but not finished, do not enter

--- app/assets/javascripts/angular/controllers/ScoreKeepersCtrl.js
+++ app/assets/javascripts/angular/controllers/ScoreKeepersCtrl.js
@@ -1,4 +1,6 @@
   app.controller('ScoreKeepersCtrl', ['$http',function($http){
+    var gameId = parseInt($("#game_id").attr("value"));
+    var url =
 
     //coordinate hash to store the x and y coordinates of a mouse click
     var coordinates = {
@@ -26,50 +28,24 @@
     // 6) Block
 
     function Stats(event, type){
+      this.game_id = gameId;
+      this.x = getX(event);
+      this.y = getY(event);
+
       if(type === 1){
-        this.field_goal = {
-          game_id: null,
-          x: getX(event),
-          y: getY(event),
-          made: false,
-          stat_type: 1
-        };
+        this.made = false;
+        this.stat_type = 1
       } else if(type === 2) {
-          this.field_goal = {
-          game_id: null,
-          x: getX(event),
-          y: getY(event),
-          made: true,
-          stat_type: 2
-        };
+         this.made = true;
+         this.stat_type = 2;
       } else if(type === 3) {
-        this.rebound = {
-          game_id: null,
-          x: getX(event),
-          y: getY(event),
-          stat_type: 3
-        };
+          this.stat_type = 3;
       } else if(type === 4){
-        this.steal = {
-          game_id: null,
-          x: getX(event),
-          y: getY(event),
-          stat_type: 4
-        };
+          this.stat_type = 4;
       } else if(type === 5){
-        this.turnover = {
-          game_id: null,
-          x: getX(event),
-          y: getY(event),
-          stat_type: 5
-        };
+          this.stat_type = 5;
       } else if(type === 6){
-        this.block = {
-          game_id: null,
-          x: getX(event),
-          y: getY(event),
-          stat_type: 6
-        };
+          this.stat_type = 6;
       }
     };
 
@@ -111,21 +87,29 @@
       if(type === 1){
         this.missed_fieldgoals.push(new Stats(event, this.type));
         makedots(event);
-        // debugger;
+
+        $http({
+          method: 'POST',
+          url: '/stat.json'
+          data:
+        })
+        debugger;
       } else if(type === 2){
         this.made_fieldgoals.push(new Stats(event, this.type));
-
+        makedots(event);
+        debugger;
       } else if(type === 3) {
         this.rebounds.push(new Stats(event, this.type));
-
+        makedots(event);
       } else if(type === 4) {
         this.steals.push(new Stats(event, this.type));
-
+        makedots(event);
       } else if(type === 5) {
         this.turnovers.push(new Stats(event, this.type));
-
+        makedots(event);
       } else if(type === 6){
         this.blocks.push(new Stats(event, this.type));
+        makedots(event);
       };
     };
 }]);
