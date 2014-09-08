8a45974e3b9006621e261e5799bf25c3fafe2141
Edgar Garza <egarza54@gmail.com>
2014-09-07 23:49:58 -0500

    heatmap renders maps for all stats, with clearmap feature

--- app/assets/javascripts/heat_show.js
+++ app/assets/javascripts/heat_show.js
@@ -1,47 +1,112 @@
 
 $(document).ready(function(){
-  var pathname = window.location.pathname;
-  var data_fieldgoals = [];
 
-  var ajaxRequest = $.ajax({
-    type: 'GET',
-    url: pathname+".json"
-  });
+    // $(".shots_missed").show();
+    // $(".shots_made").hide();
+    // $(".rebounds").hide();
+    // $(".steals").hide();
+    // $(".blocks").hide();
+    // $(".turnovers").hide();
 
-  ajaxRequest.done(function(response){
-    data_fieldgoals = response;
-    setUpHeatmap();
-  });
+    var pathname = window.location.pathname;
+    var data_fieldgoals = [];
 
-
-  var setUpHeatmap = function() {
-    var heatmapInstance = h337.create({
-      container: document.querySelector('.heat_court'),
+    var ajaxRequest = $.ajax({
+      type: 'GET',
+      url: pathname+".json"
     });
 
-    var fieldgoals = data_fieldgoals;
+    ajaxRequest.done(function(response){
+      dataMissedShots = response.missed_shots;
+      dataMadeShots = response.made_shots;
+      dataRebounds = response.rebounds;
+      dataSteals = response.steals;
+      dataTurnovers = response.turnovers;
+      dataBlocks = response.blocks;
+    });
+
+    $(".identifiers").on('click', '.fg_missed_button' ,function(){
+      setUpHeatmap(dataMissedShots);
+      // $(this).addClass("on_missed")
+    });
+
+    $(".identifiers").on('click', '.fg_made_button' ,function(){
+      setUpHeatmap(dataMadeShots);
+
+      // $(this).addClass("on_made")
+    });
+
+    $(".identifiers").on('click', '.rebound_button' ,function(){
+      setUpHeatmap(dataRebounds);
+      // $(this).addClass("on_rebound")
+    });
+
+    $(".identifiers").on('click', '.steal_button' ,function(){
+      setUpHeatmap(dataSteals);
+      // $(this).addClass("on_steal")
+    });
+
+    $(".identifiers").on('click', '.turnover_button' ,function(){
+      setUpHeatmap(dataTurnovers);
+      // $(this).addClass("on_turnover")
+    });
+
+    $(".identifiers").on('click', '.block_button' ,function(){
+      setUpHeatmap(dataBlocks);
+      // $(this).addClass("on_block")
+    });
+
+    $(".identifiers").on("click", ".clear_heatmap", function(){
+      $('.heat_court').empty();
+      // $('.heat_court').removeClass("on")
+    });
+
+  });
+
+
+
+
+  var setUpHeatmap = function(stat) {
+
+  var heatmapInstance = h337.create({
+    container: document.querySelector('.heat_court'),
+  });
+    // 1) Shot missed
+    // 2) Shot made
+    // 3) Rebound
+    // 4) Steal
+    // 5) Turnover
+    // 6) Block
+
+    // var missedFieldgoals = dataMissedShots;
+    // var madeFieldgoals = dataMadeShots;
+    // var rebounds = dataRebounds;
+    // var steals = dataSteals;
+    // var turnovers = dataTurnovers;
+    // var blocks = dataBlocks;
+    var statNum = stat.length;
+
     var points = [];
     var max = 100;
     var width = 1200;
     var height = 750;
-    var fieldgoalNum = fieldgoals.length;
 
-    var setVal = function(shotMade){
-      if(shotMade){
-        return 50;
-      } else {
-        return -50;
-      }
-    };
+    // var setVal = function(shotMade){
+    //   if(shotMade){
+    //     return 50;
+    //   } else {
+    //     return 0;
+    //   }
+    // };
 
-    while ( fieldgoalNum-- ) {
-      var val = setVal(fieldgoals[fieldgoalNum].made);
+    while ( statNum-- ) {
+      var val = 50//setVal(missedFieldgoals[missedfieldgoalNum].made);
       var radius = 40;
       max = Math.max(max, val);
 
       var point = {
-        x: fieldgoals[fieldgoalNum].x_coord,
-        y: fieldgoals[fieldgoalNum].y_coord,
+        x: stat[statNum].x_coord,
+        y: stat[statNum].y_coord,
         value: val,
         radius: radius
       };
@@ -56,7 +121,7 @@ $(document).ready(function(){
     heatmapInstance.setData(data);
   };
 
-});
+
 
 // var data_fieldgoals = [{
 //   made: true,
